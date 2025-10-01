e995452f915834da131e6ecc18431e4d203d6e8e
David <swsyjhyj@gmail.com>
2025-09-30 20:05:27 -0400

    Add choices to questions and update admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
